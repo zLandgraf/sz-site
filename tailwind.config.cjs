4110ec8902f0d1df2639ca56379c9b52ea8f474a
Alexandre Henrique Landgraf <alexandrehlandgraf@gmail.com>
2023-03-24 21:43:51 -0300

    adicionando fonte, e header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: [
+					"Agrandir Medium, sans-serif"],
+    	},
+			container: {
+				padding: '4rem',
+				center: true,
+			},
+		},
 	},
 	plugins: [],
 }
